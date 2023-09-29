d3ceaa794282643dbf4379db93c662e7b6bf6305
Muhammed Ajmal S <majmalmdt@gmail.com>
2023-09-29 10:35:30 +0530

    updated

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('D:\\ionaught\\AppBlocks\\docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('D:\\ionaught\\AppBlocks\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('D:\\ionaught\\AppBlocks\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
-  require('D:\\ionaught\\AppBlocks\\docs\\src\\css\\custom.css'),
-];
